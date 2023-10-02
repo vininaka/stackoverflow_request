da37f147509965bb1910d738358e75e1946ccf61
Vinícius Nakasone <66442324+vininaka@users.noreply.github.com>
2023-10-02 11:10:25 -0300

    feat: Code optimization
    feat: Optimized code for better readability

--- executer.py
+++ executer.py
@@ -3,37 +3,34 @@ import re
 import argparse
 from fetcher import show_solutions, get_and_print_first_solution
 
+def run_script(script_name):
+    try:
+        # Runs external script and checks if it returns an error
+        command = ["python", script_name]
+        subprocess.check_output(command, stderr=subprocess.STDOUT, universal_newlines=True)
+
+    except subprocess.CalledProcessError as e:
+        error_name = e.output.strip().splitlines()[-1]
+        match = re.match(r'(\w+Error):\s(.+$)', error_name)
+
+        if match:
+            error_type, error_details = match.groups()
+        else:
+            error_type = error_name
+            error_details = None
+
+        print(f"Error Type: {error_type}")
+        print(f"Error Details: {error_details}")
+
+        show_solutions(error_type)
+        get_and_print_first_solution(error_type)
+
 if __name__ == "__main__":
     # Create an argument parser
     parser = argparse.ArgumentParser(description="Script parser to execute")
     # Define command-line arguments
-    parser.add_argument("--script_name", type=str, help="Script to execute")
+    parser.add_argument("--script_name", type=str, required=True, help="Script to execute")
     # Parse the command-line arguments
     args = parser.parse_args()
-    # Access and use the argument values
-    value = args.script_name
-    
-    assert value is not None, "Argument 'script_name' is required and cannot be None."
 
-    try:
-        # Runs external script and checks if returns an error
-        command = ["python", value]
-        subprocess.check_output(command, stderr=subprocess.STDOUT, universal_newlines=True)
-
-    except subprocess.CalledProcessError as e:
-        # Process error format to split in error type and error details
-        error_name = e.output.strip().splitlines()
-        error_name = error_name[-1]
-
-        match = re.match(r'(\w+Error):\s(.+$)', error_name)
-        if match:
-            error_type, error_details = match.groups()
-            print(f"Error Type: {error_type}")
-            print(f"Error Details: {error_details}")
-
-            show_solutions(error_type)
-            get_and_print_first_solution((error_type))
-        else:
-            show_solutions(error_name)
-            get_and_print_first_solution((error_name))
-
+    run_script(args.script_name)
--- fetcher.py
+++ fetcher.py
@@ -4,65 +4,67 @@ import re
 from selenium import webdriver
 
 def get_solutions_from_stackoverflow(error_name):
-    print("\nSearching for solutions on StackOverflow...\n")
     BASE_URL = "https://stackoverflow.com"
     QUERY = f"/search?q={error_name}"
 
     try:
         response = requests.get(BASE_URL + QUERY)
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, "html.parser")
+        response.raise_for_status()  # Raise an exception for unsuccessful requests
+        soup = BeautifulSoup(response.text, "html.parser")
 
-            # Pattern to select answers summary
-            pattern = re.compile('question-summary|answer-id')
-            question_summary = soup.find_all('div', id=pattern)
+        # Pattern to select question summaries and answers
+        pattern = re.compile('question-summary|answer-id')
+        question_summary = soup.find_all('div', id=pattern)
 
-            answers = []
-            for summary in question_summary:
-                # Pattern to select title, number of votes and question time
-                pattern_title = re.compile('s-link|answer-hyperlink')
-                title = summary.find("a", class_=pattern_title)
-                votes = summary.find("span", class_='s-post-summary--stats-item-number')
-                time = summary.find("span", class_='relativetime')
-                
-                answers.append({
-                    'title': title.get_text(),  
-                    'link': BASE_URL + title['href'],
-                    'votes': votes.text,
-                    'date': time.text
-                })
-            return answers
+        answers = []
+        for summary in question_summary:
+            # Pattern to select title, number of votes, and question time
+            pattern_title = re.compile('s-link|answer-hyperlink')
+            title = summary.find("a", class_=pattern_title)
+            votes = summary.find("span", class_='s-post-summary--stats-item-number')
+            time = summary.find("span", class_='relativetime')
+
+            answers.append({
+                'title': title.get_text(),
+                'link': BASE_URL + title['href'],
+                'votes': votes.text,
+                'date': time.text
+            })
+        return answers
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred while fetching solutions: {e}")
+        return []
 
 def show_solutions(error_name):
-    solutions = get_solutions_from_stackoverflow(error_name)
-    print("================================StackOveflow Solutions ========================================================\n")
-    for answer in solutions:
-        print("Title: ", answer['title'])
-        print("Link: ", answer['link'])
-        print("Number of Votes: ", answer['votes'])
-        print("Date: ", answer['date'])
-        print('\n')
-
-def get_and_print_first_solution(error_name):
-    driver = webdriver.Chrome()
+    print("\nSearching for solutions on StackOverflow...\n")
     solutions = get_solutions_from_stackoverflow(error_name)
     if solutions:
-        first_solution_url = solutions[1]['link']
+        print("================================ StackOverflow Solutions ================================================\n")
+        for answer in solutions:
+            print("Title: ", answer['title'])
+            print("Link: ", answer['link'])
+            print("Number of Votes: ", answer['votes'])
+            print("Date: ", answer['date'])
+            print('\n')
+    else:
+        print("No solutions found for the error.")
+
+def get_and_print_first_solution(error_name):
+    print("\nFetching and saving a screenshot of the first solution...\n")
+    solutions = get_solutions_from_stackoverflow(error_name)
+    if solutions:
+        first_solution_url = solutions[0]['link']
         try:
+            driver = webdriver.Chrome()
             driver.get(first_solution_url)
             driver.save_screenshot("screenshot.png")
+            driver.quit()  # Close the WebDriver after use
         except Exception as e:
             print(f"An error occurred while fetching the first solution: {e}")
     else:
         print("No solutions found for the error.")
 
-
 if __name__ == "__main__":
-    get_and_print_first_solution("ValueError")
-
-    
-    
-    
+    error_name = "ValueError"  # Replace with your desired error name
+    show_solutions(error_name)
+    get_and_print_first_solution(error_name)
